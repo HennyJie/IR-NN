e7a1251bba97d73bda2b761a17708f3c07986b88
hejie <cuihejie331771@gmail.com>
2020-03-22 22:48:59 -0400

    nn model v1

--- FullyConnectedNN.py
+++ FullyConnectedNN.py
@@ -14,23 +14,36 @@ import torch.optim as optim
 import pandas as pd
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
+from torch.utils.data import TensorDataset
+import numpy as np
 
-epochs = 10
-batch_size = 200
-learning_rate = 0.01
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+input_size = 46
+hidden_size = 128
+num_classes = 3
+
+num_epochs = 10
+batch_size = 32
+learning_rate = 1e-4
 log_interval = 10
 
 
 class Dataset(Dataset):
     def __init__(self, filename):
         with open(filename) as f:
-            lines = f.read().split('\n')
+            lines = f.read().split('\n')[:-1]
         X, y = [], []
         for line in lines:
-            X.append(line.split(' ')[2:48])
-            y.append(line.split(' ')[0])
-        self.X = X
-        self.y = y
+            X.append([float(item.split(':')[1])
+                      for item in line.split(' ')[2:48]])
+            for label in line.split(' ')[0]:
+                one_hot_label = [0 for _ in range(num_classes)]
+                one_hot_label[int(label)] = 1
+                y.append(one_hot_label)
+
+        self.X = torch.from_numpy(np.array(X))
+        self.y = torch.from_numpy(np.array(y))
 
     def __len__(self):
         return len(self.y)
@@ -40,69 +53,72 @@ class Dataset(Dataset):
 
 
 class Net(nn.Module):
-    def __init__(self, input_dim=46, output_dim=3):
+    def __init__(self, input_size=46, hidden_size=128, num_classes=3):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(input_dim, 200)
-        self.fc2 = nn.Linear(200, 200)
-        self.fc3 = nn.Linear(200, output_dim)
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return F.log_softmax(x)
+        out = self.fc3(x)
+        return out
 
 
 def run_nn():
     train_dataset = Dataset(
-        'MQ2007/Fold1/train.txt')
-    test_dataset = Dataset(
-        'MQ2007/Fold1/test.txt')
-    train_loader = DataLoader(train_dataset, batch_size=200, num_workers=5)
-    test_loader = DataLoader(test_dataset, batch_size=200, num_workers=5)
+        '/home/xuankan/Documents/IR-NN/MQ2007/Fold1/train.txt')
+    train_dataset = TensorDataset(train_dataset.X, train_dataset.y)
+    train_loader = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
 
-    net = Net()
-    print(net)
-    if torch.cuda.is_available():
-        net.cuda()
+    test_dataset = Dataset(
+        '/home/xuankan/Documents/IR-NN/MQ2007/Fold1/test.txt')
+    test_dataset = TensorDataset(test_dataset.X, test_dataset.y)
+    test_loader = DataLoader(
+        test_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+    net = Net(input_size, hidden_size, num_classes).to(device)
 
     optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9)
-    criterion = nn.NLLLoss()
+    criterion = nn.CrossEntropyLoss()
 
-    for epoch in range(epochs):
+    total_step = len(train_loader)
+    for epoch in range(num_epochs):
         for batch_idx, (data, target) in enumerate(train_loader):
-            print(len(data))
-            print(target.shape)
-            if torch.cuda.is_available():
-                data, target = Variable(data.cuda()), Variable(target.cuda())
-            else:
-                data, target = Variable(data), Variable(target)
+            data = data.to(device)
+            target = target.to(device)
 
-            data = data.View(-1, 46)
+            net_out = net(data.float())
+            loss = criterion(net_out, torch.max(target, 1)[1])
+
             optimizer.zero_grad()
-            net_out = net(data)
-            loss = criterion(net_out, target)
             loss.backward()
             optimizer.step()
+
             if batch_idx % log_interval == 0:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, batch_idx * len(data), len(train_loader.dataset),
-                    100. * batch_idx / len(train_loader), loss.data[0]))
+                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                      .format(epoch+1, num_epochs, batch_idx+1, total_step, loss.item()))
 
-    test_loss = 0
-    correct = 0
-    for data, target in test_loader:
-        data, target = Variable(data, volatile=True), Variable(target)
-        data = data.view(-1, 46)
-        net_out = net(data)
-        test_loss += criterion(net_out, target).data[0]
-        pred = net_out.data.max(1)[1]
-        correct += pred.eq(target.data).sum()
+    with torch.no_grad():
+        test_loss = 0
+        correct = 0
+        for data, target in test_loader:
+            data = data.to(device)
+            target = target.to(device)
 
-    test_loss /= len(test_loader.dataset)
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+            net_out = net(data.float())
+            test_loss += criterion(net_out, torch.max(target, 1)[1]).data
+            pred = net_out.data.max(1)[1]
+            correct += pred.eq(torch.max(target, 1)[1].data).sum()
+
+        test_loss /= len(test_loader.dataset)
+        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+            test_loss, correct, len(test_loader.dataset),
+            100. * correct / len(test_loader.dataset)))
+
+    torch.save(net.state_dict(), 'model.ckpt')
 
 
 if __name__ == "__main__":
